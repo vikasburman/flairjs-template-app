a5834bcc67bc7232e3f52bd3bf74c463a29e45f1
Vikas Burman <vikasburman@gmail.com>
2017-09-09 18:32:20 +0530

    fixed

--- sys/modules/core/members/comm/Handler.js
+++ sys/modules/core/members/comm/Handler.js
@@ -24,16 +24,19 @@ define([
         this.func('handle', (request) => {
             let errorText = (!this.env.isServer ? `Error handling: ${request.url}#${request.verb}.` : `Error handling: ${request.url}.`);
             include([use(this.className)]).then((Handler) => {
-                let handler = new Handler();
-                let handlerInfo = Reflector.get(handler);
-                let funcInfo = handlerInfo.getMember(this.funcName);
+                let handler = new Handler(),
+                    fn = getNestedKeyValue(handler, this.funcName, null);
                 this.env.set('currentRequest', request);
-                handler[this.funcName](request).then((result) => {
-                    this.env.reset('currentRequest');
-                }).catch((err) => {
-                    this.env.reset('currentRequest');
-                    this.onError(err, errorText);
-                });   
+                if (fn) {
+                    fn(request).then((result) => {
+                        this.env.reset('currentRequest');
+                    }).catch((err) => {
+                        this.env.reset('currentRequest');
+                        this.onError(err, errorText);
+                    });   
+                } else {
+                    this.onError(`Invalid service handler. (${this.className}.${this.funcName})`, errorText);
+                }
             }).catch((err) => {
                 this.onError(err, errorText);
             });
