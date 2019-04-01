8c6dfeb6fba3709f927642a8ba7cc95218ba5a43
Vikas Burman <vikasburman@gmail.com>
2019-03-31 20:04:59 -0400

    ...

--- src/server/.deploy/firebase/main.firebase.js
+++
@@ -1,5 +0,0 @@
-(async () => {
-    const functions = require("firebase-functions");
-    const mainApp = await require('./main.js');
-    module.exports = functions.https.onRequest(mainApp);
-})();
