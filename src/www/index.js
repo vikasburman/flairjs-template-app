40024fe1d04265fe522218d502b518c2c0b579c8
Vikas Burman <vikasburman@gmail.com>
2019-05-13 20:47:11 -0400

    ...

--- src/www/index.js
+++ src/www/index.js
@@ -3,7 +3,7 @@
  * Your App Description
  * Copyright Message
  */
-const __filename = (typeof document !== 'undefined' ? document.currentScript.src : ''); // to support web worker
+const __filename = (typeof document !== 'undefined' ? document.currentScript.src : ''); // to consider web worker env as well
 require(['./modules/flairjs/flair.js'], (flair) => {
     flair(__filename, './webConfig.json').then((app) => {
         console.log('*');
