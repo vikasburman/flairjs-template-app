7ec9bc734116df6e570c0b23ab1985add973764e
Vikas Burman <vikasburman@gmail.com>
2019-08-08 22:42:41 -0400

    ...

--- src/www/features/myapp.feature1/myapp.feature1.services/ServerDateTime.js
+++ src/www/features/myapp.feature1/myapp.feature1.services/ServerDateTime.js
@@ -5,7 +5,7 @@
 $$('ns', '(auto)');
 $$('static');
 Class('(auto)', function() {
-    $$('fetch', 'get', 'json', '/**/api/now');
+    $$('fetch', 'get', 'json', '/**/api/now', 'cachePolicyType1');
     this.now = async (api) => {
         let result = await api() || { now: 'Could not connect to server.' };
         return result.now;
