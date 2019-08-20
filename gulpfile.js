7f918b78738f774521366c6c747806bf87da6e65
Vikas Burman <vikasburman@gmail.com>
2019-08-19 22:45:23 -0400

    ...

--- gulpfile.js
+++ gulpfile.js
@@ -9,15 +9,15 @@ gulp.task('build-full', (done) => {
     require(gulpOptions.build).build(done);
 });
 
+// task: flag
+gulp.task('flag', (done) => {
+    require(gulpOptions.flag).flag(done);
+});
+
 // task: test
 gulp.task('test', (done) => {
     require(gulpOptions.test).test(done);
 });
 gulp.task('test-client', (done) => {
     require(gulpOptions.test).test(done);
-});
-
-// task: package
-gulp.task('package', (done) => {
-    require(gulpOptions.package).pack(done);
 });
