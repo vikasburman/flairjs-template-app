d49ac6fc7315dc494a1556c790e90496ad7bd4b0
Vikas Burman <vikasburman@gmail.com>
2019-08-30 10:14:08 -0400

    ...

--- src/server/main.js
+++
@@ -1,9 +0,0 @@
-/**
- * Your App - Server
- * Your App Description
- * Copyright Message
- */
-const start = require('flairjs-fabric/flair.server/start.js');
-start(__dirname, __filename, (flair, app) => {
-    console.log('!'); // application is started now
-});
