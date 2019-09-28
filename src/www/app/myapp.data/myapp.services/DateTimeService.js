4072d5ba073a255ded65b12e6cc5634131fec547
Vikas Burman <vikasburman@gmail.com>
2019-09-27 22:02:47 -0400

    ...

--- src/www/app/myapp.data/myapp.services/DateTimeService.js
+++ src/www/app/myapp.data/myapp.services/DateTimeService.js
@@ -4,28 +4,29 @@
  */
 $$('static');
 Class('', function() {
-    $$('cache', 10000);
-    this.now = async (type = 'ms') => {
-        // pure client way of keeping data logic separate 
-        // on an app that connects to server it can fetch data from server
-        // as shown below
-        let result = '';
-        switch (type) {
-            case 'ms': result = Date.now().toString(); break;
-            case 'iso': result = new Date().toISOString(); break;
-            case 'utc': result = new Date().toUTCString(); break;
-            default: result = new Date().toJSON().toString(); break;
-        }
-        return result;
-    };
 
     // $$('cache', 10000);
-    // $$('fetch', 'app-server', 'get-json', '/now/:format');
-    // this.now = async (api, format = 'ms') => {
-    //     let apiArgs = {
-    //         format: format
-    //     };
-    //     let result = await api(apiArgs);
-    //     return result || 'Could not connect to server.';
+    // this.now = async (type = 'ms') => {
+    //     // pure client way of keeping data logic separate 
+    //     // on an app that connects to server it can fetch data from server
+    //     // as shown below
+    //     let result = '';
+    //     switch (type) {
+    //         case 'ms': result = Date.now().toString(); break;
+    //         case 'iso': result = new Date().toISOString(); break;
+    //         case 'utc': result = new Date().toUTCString(); break;
+    //         default: result = new Date().toJSON().toString(); break;
+    //     }
+    //     return result;
     // };
+
+    $$('cache', 10000);
+    $$('fetch', 'api-server', 'get', 'json', '/now/:format?');
+    this.now = async (api, format = 'ms') => {
+        let args = {
+            format: format
+        };
+        let result = await api(args);
+        return (result.isError ? result.message : result.data);
+    };
 });
